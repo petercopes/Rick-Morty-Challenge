ac0ac50b6a0e5346005f7d353c5aa1a131a439ec
Pedro Copes <petercopes.96@gmail.com>
2026-03-19 11:56:14 -0300

    added context injection and eslint config

--- thrive-challenge-copes/src/pages/_document.tsx
+++ thrive-challenge-copes/src/pages/_document.tsx
@@ -1,21 +1,62 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>The Portal-Dex</title>
-        <meta
-          name="description"
-          content="Catalog every character, alien, and dimension in the show"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/site-icon.png" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="Catalog every character, alien, and dimension in the show"
+          />
+          <link rel="icon" href="/site-icon.png" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Consolas:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- thrive-challenge-copes/src/types/styled.d.ts
+++ thrive-challenge-copes/src/types/styled.d.ts
@@ -3,5 +3,6 @@ import { theme } from '../styles/theme';
 type ThemeType = typeof theme;
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends ThemeType {}
 }
